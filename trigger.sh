127415412a74d41f54c93008b252f88afcadebcf
Christian Meter <cmeter@googlemail.com>
2023-05-17 10:36:00 +0200

    Cycle all ports

--- trigger.sh
+++ trigger.sh
@@ -5,4 +5,4 @@ echo "$(date): Camera disconnected" >> /opt/reboot-usb-devices/logs/reboot-usb-d
 # -p: port numbers
 # -l: hub number
 # -a: action
-sudo uhubctl -a cycle -p 1-4 -l 2
+sudo uhubctl -a cycle -l 2
