715c2a688691c6f9074f4ef3ae53ce229f4ede53
Christian Meter <cmeter@googlemail.com>
2023-05-19 15:46:50 +0200

    Fix udev rule

--- trigger.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "$(date): Camera disconnected" >> /opt/reboot-usb-devices/logs/reboot-usb-devices.log
-
-# -p: port numbers
-# -l: hub number
-# -a: action
-sudo uhubctl -a cycle -l 2
