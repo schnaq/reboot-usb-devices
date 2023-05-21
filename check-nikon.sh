e153d89e66fbc43eaaa82dee5080dfc26f1ba28f
Christian Meter <cmeter@googlemail.com>
2023-05-21 12:50:21 +0200

    Add while loop

--- check-nikon.sh
+++ check-nikon.sh
@@ -2,10 +2,15 @@
 
 echo "$(date): Checking for Nikon Cameras" >> /opt/reboot-usb-devices/logs/reboot-usb-devices.log
 
-gphoto2 --auto-detect | grep Nikon
-if [ $? -eq 0 ]
-then
-  echo "$(date): Nikon found" >> /opt/reboot-usb-devices/logs/reboot-usb-devices.log
-else
-  echo "$(date): Nikon not found" >> /opt/reboot-usb-devices/logs/reboot-usb-devices.log
-fi
+while true
+do
+
+  gphoto2 --auto-detect | grep Nikon
+  if [ $? -eq 0 ]
+  then
+    echo "$(date): Nikon found" >> /opt/reboot-usb-devices/logs/reboot-usb-devices.log
+  else
+    echo "$(date): Nikon not found" >> /opt/reboot-usb-devices/logs/reboot-usb-devices.log
+  fi
+  sleep 1
+done
