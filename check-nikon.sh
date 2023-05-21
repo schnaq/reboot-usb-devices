b7b5fe3240ee5354012e9dcf6b8aca742d827e82
Christian Meter <cmeter@googlemail.com>
2023-05-21 16:49:14 +0200

    Less prints, more sleep

--- check-nikon.sh
+++ check-nikon.sh
@@ -8,10 +8,10 @@ do
   gphoto2 --auto-detect | grep Nikon
   if [ $? -eq 0 ]
   then
-    echo "$(date): Nikon found" >> /opt/reboot-usb-devices/logs/reboot-usb-devices.log
+    echo "$(date): Nikon found" # >> /opt/reboot-usb-devices/logs/reboot-usb-devices.log
   else
     echo "$(date): Nikon not found" >> /opt/reboot-usb-devices/logs/reboot-usb-devices.log
     /bin/bash /opt/reboot-usb-devices/reboot-usb.sh &
   fi
-  sleep 1
+  sleep 5
 done
