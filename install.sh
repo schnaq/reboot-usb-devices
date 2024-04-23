5a8abc6491757f048cdba278f5761bf30bd68f5f
Christian Meter <cmeter@googlemail.com>
2024-04-23 10:41:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ printf "\n# This script installs uhubctl\n"
 
 sudo apt-get install -y libusb-1.0-0-dev
 
-git clone https://github.com/mvp/uhubctl.git
+sudo git clone https://github.com/mvp/uhubctl.git
 cd uhubctl
 make
 sudo make install
