12e6339b1718f833f46975e0cd25c9b0165e45fb
Christian Meter <cmeter@googlemail.com>
2024-07-04 11:35:25 +0200

    Update reboot-usb.sh

--- reboot-usb.sh
+++ reboot-usb.sh
@@ -7,8 +7,8 @@ if [ ! -f "$FILE" ]
 then
   echo "$(date): Restarting USB Hub" >> $BASE/logs/reboot-usb-devices.log
   echo 1 >> $FILE
-  /sbin/uhubctl -a cycle -l 2 # >> $BASE/logs/reboot-usb-devices.log
-  /sbin/uhubctl -a cycle -l 1 # >> $BASE/logs/reboot-usb-devices.log
+  sudo /sbin/uhubctl -a cycle # >> $BASE/logs/reboot-usb-devices.log
+  # /sbin/uhubctl -a cycle -l 1 # >> $BASE/logs/reboot-usb-devices.log
   rm $FILE
 else
   echo "$(date): Skipping" >> $BASE/logs/reboot-usb-devices.log
